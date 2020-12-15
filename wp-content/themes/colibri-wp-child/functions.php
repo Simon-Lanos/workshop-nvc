eb71b7dc0d46c5f3400cc5d4a0b1aa0e73426900
Simon LANOS <simon.lanos99@gmail.com>
2020-12-15 12:10:22 +0100

    feat(): ajoute le thème enfant pour colibri-wp et active le thème

---
+++ wp-content/themes/colibri-wp-child/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * activation theme
+ */
+
+add_action(
+    'wp_enqueue_scripts',
+    static function () {
+        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    }
+);
+
